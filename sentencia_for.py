e17e6d7175742f076880b9c9b8f78c357b04fb65
david0-desing <romoralesba@uide.edu.ec>
2025-12-02 22:08:36 -0500

    Add files via upload

---
+++ sentencia_for.py
@@ -0,0 +1,7 @@
+suma=0
+for i in range(10):
+    calificacion=float(input("Ingrese la calificacion: "))
+    suma=suma+calificacion
+promedio=suma/10
+print("La sumatoria es: ",suma)
+print("El promedio es:",promedio)
